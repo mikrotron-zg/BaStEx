709ecf9b504ea0694053ca6d2962c1d713d0158f
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2016-01-02 20:59:46 +0100

    Initial commit

---
+++ src/hr/mikrotron/bastex/BaStEx.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hr.mikrotron.bastex;
+
+/**
+ * @author prexy
+ *
+ */
+public class BaStEx {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
